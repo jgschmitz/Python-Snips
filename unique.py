fab4a35f090fb781efde7c9d1e5dd78cff6eb1eb
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2019-09-29 14:36:03 -0500

    Create unique.py

---
+++ unique.py
@@ -0,0 +1,10 @@
+Python3
+#The following method checks whether the given list has duplicate elements. It uses the property of set() which removes duplicate elements from the list.
+def all_unique(lst):
+    return len(lst) == len(set(lst))
+
+
+x = [1,1,2,2,3,2,3,4,5,6]
+y = [1,2,3,4,5]
+all_unique(x) # False
+all_unique(y) # True
