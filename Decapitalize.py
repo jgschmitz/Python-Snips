9c78ca53f701694503d600c247542365d43a6bd5
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2019-09-30 09:19:13 -0500

    Create Decapitalize.py

---
+++ Decapitalize.py
@@ -0,0 +1,7 @@
+#This method can be used to turn the first letter of the given string into lowercase.
+def decapitalize(str):
+    return str[:1].lower() + str[1:]
+  
+  
+decapitalize('FooBar') # 'fooBar'
+decapitalize('FooBar') # 'fooBar'
