1f4b92c7f78682e9c4fb0cf5be83573d9a864b5e
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2019-09-30 10:24:44 -0500

    Create Diff.py

---
+++ Diff.py
@@ -0,0 +1,11 @@
+python3
+#This method finds the difference between two iterables by keeping only the values that are in the first one.
+def difference(a, b):
+    set_a = set(a)
+    set_b = set(b)
+    comparison = set_a.difference(set_b)
+    return list(comparison)
+
+
+difference([1,2,3], [1,2,4]) # [3]
+
