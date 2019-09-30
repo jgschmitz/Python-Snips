e7c30727f2504a7ef74c7fd218457678058e4b69
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2019-09-30 09:16:59 -0500

    Create commentSeperated.py

---
+++ commentSeperated.py
@@ -0,0 +1,5 @@
+#This snippet can be used to turn a list of strings into a single string with each element from the list separated by commas.
+hobbies = ["basketball", "football", "swimming"]
+
+print("My hobbies are:") # My hobbies are:
+print(", ".join(hobbies)) # basketball, football, swimming
