a1656d7dffa165d18f1e70b509f00f2f27d36533
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2019-09-30 09:08:40 -0500

    Create removeFalsy.py

---
+++ removeFalsy.py
@@ -0,0 +1,6 @@
+#This method removes falsy values (False, None, 0 and “”) from a list by using filter().
+def compact(lst):
+    return list(filter(bool, lst))
+  
+  
+compact([0, 1, False, 2, '', 3, 'a', 's', 34]) # [ 1, 2, 3, 'a', 's', 34 ]
