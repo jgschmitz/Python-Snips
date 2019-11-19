0600cb3a8fed5ccd57c541a32257511f6e9231d8
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2019-11-19 15:27:10 -0600

    Create timcalc.py

---
+++ timcalc.py
@@ -0,0 +1,7 @@
+import time
+start_time = time.time()
+a,b = 5,10
+c = a+b
+end_time = time.time()
+time_taken = (end_time- start_time)*(10**6)
+print("Time taken in micro_seconds:", time_taken) # Time taken in micro_seconds: 39.577484130859375
