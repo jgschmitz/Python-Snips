f05df2c7d4bc092a022872701e5df9f8d9485395
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-12-24 07:27:54 -0600

    Update anagram.py

--- anagram.py
+++ anagram.py
@@ -2,7 +2,7 @@ python3
 
 #This method can be used to check if two strings are anagrams. An anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 from collections import Counter
-print 1,2,3,4,5,6,7,8,9,10
+print 1,2,3,4,5,6,7,8,9,
 def anagram(first, second):
     return Counter(first) == Counter(second)
 
