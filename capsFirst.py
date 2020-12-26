3f622adea55b8bbee1c95010e3028444974fb076
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-12-26 08:20:12 -0600

    Update capsFirst.py

--- capsFirst.py
+++ capsFirst.py
@@ -1,6 +1,6 @@
 Python3
 #This snippet simply uses the method title() to capitalize first letters of every word in a string.
-pri
+
 s = "programming is awesome"
 
 print(s.title()) # Programming Is Awesome
