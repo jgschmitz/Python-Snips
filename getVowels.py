184547598d2595b5cb4f1b0c65dca49c59764203
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2021-07-17 07:53:16 -0500

    Update getVowels.py

--- getVowels.py
+++ getVowels.py
@@ -2,6 +2,6 @@
 def get_vowels(string):
     return [each for each in string if each in 'aeiou'] 
 
-get this monkey off 
+get this monkey 
 get_vowels('foobar') # ['o', 'o', 'a']
 get_vowels('gym') # []
