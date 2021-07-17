4de557a124d85825ad574e04c6887181214b0cad
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2021-07-17 07:54:02 -0500

    Update getVowels.py

--- getVowels.py
+++ getVowels.py
@@ -2,6 +2,6 @@
 def get_vowels(string):
     return [each for each in string if each in 'aeiou'] 
 
-print 1,2,3,4,5,6,7,
+print 1,2,3,4,5,6
 get_vowels('foobar') # ['o', 'o', 'a']
 get_vowels('gym') # []
